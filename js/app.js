acc87f99e98f3df46a6d9e2230c92af58f4d7e03
Yura Fedoriv <yurko.fedoriv@gmail.com>
2014-06-09 22:17:04 +0300

    added handling for parsed_calories, parsed_serving, parsed_serving_qty attributes

--- js/app.js
+++ js/app.js
@@ -37,17 +37,32 @@
             function ($scope, $routeParams, TrackLogEntry) {
                 $scope.showLoader = true;
 
+                $scope.getTotalCalories = function (items) {
+                    var sum = 0;
+
+                    angular.forEach(items, function (item) {
+                        sum += item.parsed_calories || 0;
+                    });
+
+                    return sum;
+                };
+
                 TrackLogEntry.query(
                     {userId: $routeParams.userId},
                     function (entries) {
-                        var sortedData = {}, dates = [];
+                        var sortedData = {}, dates = {};
                         angular.forEach(entries, function (entry) {
                             var splittedDate = entry.created_at.split(' ');
                             if (!sortedData[splittedDate[0]]) {
                                 sortedData[splittedDate[0]] = [];
-                                dates.push(splittedDate[0]);
+                                dates[splittedDate[0]] = 0;
                             }
+
                             sortedData[splittedDate[0]].push(entry);
+                        });
+
+                        angular.forEach(dates, function(value, index){
+                            dates[index] = $scope.getTotalCalories(sortedData[index]);
                         });
 
                         $scope.dates = dates;
@@ -71,9 +86,7 @@
                 template: '<div ng-show="visible" class="container loading"><i class="fa fa-spinner fa-spin fa-5x"></i></div>',
                 replace:  true,
                 scope:    {visible: '='},
-                link:     function (scope) {
-                    console.log(scope.visible);
-                }
+                link:     function (scope) {}
             }
         }
     ])
--- protected/views/site/partials/index.php
+++ protected/views/site/partials/index.php
@@ -1,17 +1,28 @@
-<div ng-repeat="date in dates | orderBy : 'toString()' : true">
+<div ng-repeat="(date, totalCalories) in dates | orderBy : 'toString()' : true">
     <div class="row date-row">
         <div class="col-md-12">
-            <div class="total-calories">-</div>
+            <div class="total-calories">{{totalCalories || '-'}}</div>
             {{date | date: 'EEEE, M/d'}}
         </div>
     </div>
 
     <div class="row" ng-repeat="item in data[date]">
         <div class="col-md-12 item">
-            <a href="" class="btn btn-block btn-warning btn-calories">!</a>
+            <a
+                href=""
+                class="btn btn-block btn-calories"
+                ng-class="{'btn-warning': !item.parsed_calories, 'btn-info': item.parsed_calories}"
+                >
+                {{item.parsed_calories || '!'}}
+            </a>
             <h4>{{item.query}}</h4>
 
-            <p>-</p>
+            <p>
+                {{
+                item.parsed_serving && item.parsed_serving_qty ?
+                (item.parsed_serving_qty + ' ' + item.parsed_serving) : '-'
+                }}
+            </p>
         </div>
     </div>
 </div>

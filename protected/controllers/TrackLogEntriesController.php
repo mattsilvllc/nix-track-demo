812f300eba9f5d03f5551b74e46ebf73e18a7492
Yura Fedoriv <yurko.fedoriv@gmail.com>
2014-06-10 00:12:04 +0300

    filter deleted records. (server side)

--- protected/controllers/TrackLogEntriesController.php
+++ protected/controllers/TrackLogEntriesController.php
@@ -5,7 +5,12 @@
  */
 class TrackLogEntriesController extends Controller {
     public function actionQuery($userId) {
-        $entries = TrackLogEntry::model()->findAllByAttributes(['user_id' => $userId]);
+        $entries = TrackLogEntry::model()->findAllByAttributes(
+            [
+                'user_id' => $userId,
+                'deleted' => 0,
+            ]
+        );
 
         header('Content-Type: application/json');
         echo CJSON::encode($entries);
